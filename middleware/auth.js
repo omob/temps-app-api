4f14817efe2ed9fa691de282ae04e94610541097
Ayodeji Abodunrin <aboayosam@gmail.com>
2021-06-17 01:39:47 +0100

    fix invalid token error by replacing jwt secret

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ module.exports = function (req, res, next) {
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
   } catch (ex) {
